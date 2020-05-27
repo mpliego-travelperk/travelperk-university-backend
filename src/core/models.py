e9fd58800a9f738cd883d29e5e8e8da5c5647958
Martin Pliego <martin.pliego@travelperk.com>
2020-05-27 09:27:27 -0300

    Fixed typo from double quotes to single quotes.

--- src/core/models.py
+++ src/core/models.py
@@ -17,7 +17,7 @@ class Ingredient(models.Model):
     name = models.CharField(max_length=255)
     recipe = models.ForeignKey(
         Recipe,
-        related_name="ingredients",
+        related_name='ingredients',
         on_delete=models.CASCADE)
 
     def save(self, *args, **kwargs):
--- src/core/tests/test_models.py
+++ src/core/tests/test_models.py
@@ -7,29 +7,29 @@ class ModelTests(TestCase):
 
     def test_new_ingredient_str(self):
         """Test the ingredient string representation"""
-        ingredient = models.Ingredient(name="Tomato")
+        ingredient = models.Ingredient(name='Tomato')
 
         self.assertEqual(str(ingredient), ingredient.name)
 
     def test_new_recipe_str(self):
         """Test the recipe string representation"""
         recipe = models.Recipe(
-            name="Tomato Sauce",
-            description="Tomato Sauce Description")
+            name='Tomato Sauce',
+            description='Tomato Sauce Description')
 
         self.assertEqual(str(recipe), recipe.name)
 
     def test_same_name_different_recipe(self):
         """Test the ingredients of the same name are allow to be created"""
-        recipe = models.Recipe.objects.create(name="Tomato Soup")
+        recipe = models.Recipe.objects.create(name='Tomato Soup')
         recipe.save()
-        ingredient = models.Ingredient(name="Tomato")
+        ingredient = models.Ingredient(name='Tomato')
         ingredient.recipe = recipe
         ingredient.save()
 
-        recipe2 = models.Recipe.objects.create(name="Tomato Soup")
+        recipe2 = models.Recipe.objects.create(name='Tomato Soup')
         recipe2.save()
-        ingredient2 = models.Ingredient(name="Tomato")
+        ingredient2 = models.Ingredient(name='Tomato')
         ingredient2.recipe = recipe2
         ingredient2.save()
 
@@ -37,13 +37,13 @@ class ModelTests(TestCase):
 
     def test_same_name_same_recipe(self):
         """Test ingredients of the same name are not allowed to be created"""
-        recipe = models.Recipe.objects.create(name="Tomato Soup")
+        recipe = models.Recipe.objects.create(name='Tomato Soup')
         recipe.save()
-        ingredient = models.Ingredient(name="Tomato")
+        ingredient = models.Ingredient(name='Tomato')
         ingredient.recipe = recipe
         ingredient.save()
 
-        ingredient2 = models.Ingredient(name="Tomato")
+        ingredient2 = models.Ingredient(name='Tomato')
         ingredient2.recipe = recipe
         ingredient2.save()
 
